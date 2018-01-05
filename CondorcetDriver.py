9270eaa3f6cd7a519895dc1f57e1f40f26839d03
jbernard@CS.LOCAL <jbernard@CS.LOCAL>
2018-01-05 18:03:49 -0500

    pull trial generation out so we can be more DRY

--- CondorcetDriver.py
+++ CondorcetDriver.py
@@ -11,7 +11,7 @@ def generateFileName(numPlayers, numPlayersPerGame, discrepancyRange, trial):
 
 
 
-def DriveSimulator(numberOfMachines, machineNumber, firstTrial, numberOfTrials):
+def trialGenerator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, function): #@Kyle pass a function with 6 parameters to do that thing to the variables that trial generator generates
     i = machineNumber # should range between 1 and number of machines
     start = time.time()
     baseNumPlayersPerGame = [3, 4, 5]
@@ -19,7 +19,7 @@ def DriveSimulator(numberOfMachines, machineNumber, firstTrial, numberOfTrials):
     timeLimitMinutes = 2
     timeLimit = timeLimitMinutes*60
     discrepancyRangeList = [280, 560, 1120]
-    baseNumPlayers = [10, 20, 40, 70, 130, 250]
+    baseNumPlayers = [1, 10, 20, 40, 70, 130, 250]
     for discrepancyIdx in range(0, len(discrepancyRangeList)):
         thisDiscrepancyRange = discrepancyRangeList[discrepancyIdx]
         for numPlayersPerGameIdx in range(0, len(baseNumPlayersPerGame)):
@@ -34,14 +34,17 @@ def DriveSimulator(numberOfMachines, machineNumber, firstTrial, numberOfTrials):
                     if (i%numberOfMachines == 0):
                         print("number of players = ", thisNumPlayers, "number of games = ", thisNumGames, "discrepancy = ", thisDiscrepancyRange, "number of players per game = ", thisNumPlayersPerGame)
                         fileName = generateFileName(thisNumPlayers, thisNumPlayersPerGame, thisDiscrepancyRange, trial)
-                        Condorcet.simulate(thisNumPlayers, thisNumGames, thisNumPlayersPerGame, thisDiscrepancy, fileName, timeLimit)
+                        function(thisNumPlayers, thisNumGames, thisNumPlayersPerGame, thisDiscrepancy, fileName, timeLimit)
                     i+=1
     end = time.time()
     print(end-start)
 
-
+def DriveSimulator(numberOfMachines, machineNumber, firstTrial, numberOfTrials):
+    trialGenerator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, Condorcet.simulate)
 
 DriveSimulator(4, 4, 6, 1)
+
+
 
 
             
