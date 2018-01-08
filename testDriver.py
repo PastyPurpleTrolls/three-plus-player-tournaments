db2a01dba6483417bfcf4d5595661eddac952744
kshelton <kshelton@CS.LOCAL>
2018-01-08 15:34:57 -0500

    modified .py

--- testDriver.py
+++ testDriver.py
@@ -1,13 +1,13 @@
 import calculatePower
 import CondorcetDriver
 
-numberOfMachines = 1
+numberOfMachines = 6
 machineNumber = 1
 firstTrial = 1
 numberOfTrials = 5
-baseNumPlayersPerGame = [3, 4, 5]
-discrepancyRangeList = [280, 560, 1120]
-baseNumPlayers = [10, 20, 40, 70, 130, 250]
+baseNumPlayersPerGame = [4]
+discrepancyRangeList = [560]
+baseNumPlayers = [4, 8, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 250, 500, 1000]
 CondorcetDriver.DriveSimulator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, baseNumPlayersPerGame, discrepancyRangeList, baseNumPlayers)
 CondorcetDriver.trialGenerator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, baseNumPlayersPerGame, discrepancyRangeList, baseNumPlayers, calculatePower.calculateLeastSquares)
 
