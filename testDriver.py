a846b836fa139014a47926bc0d1010c355079122
kshelton@CS.LOCAL <kshelton@CS.LOCAL>
2018-01-10 10:55:35 -0500

    graphs now work! yaaaay!

--- testDriver.py
+++ testDriver.py
@@ -5,14 +5,13 @@ numberOfMachines = 1
 machineNumber = 1
 firstTrial = 1
 numberOfTrials = 1
-baseNumPlayersPerGame = [8]
-discrepancyRangeList = [50000]
-baseNumPlayers = [4]
+baseNumPlayersPerGame = [4]
+discrepancyRangeList = [560]
+baseNumPlayers = [4, 8, 10, 20, 30, 40]
 numGames = 100000
 timeLimitMinutes = 5
 targetVar = 0 ##0: player number, 1: player number per game, 2: disparity
 directory = "CondorcetData/"
 CondorcetDriver.DriveSimulator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, baseNumPlayersPerGame, discrepancyRangeList, baseNumPlayers, numGames, timeLimitMinutes, directory, targetVar)
-CondorcetDriver.trialGenerator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, baseNumPlayersPerGame, discrepancyRangeList, baseNumPlayers, numGames, timeLimitMinutes, directory, targetVar, calculatePower.calculateLeastSquares)
-CondorcetDriver.trialGenerator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, baseNumPlayersPerGame, discrepancyRangeList, baseNumPlayers, numGames, timeLimitMinutes, directory, targetVar, calculatePower.calculateLeastSquares)
+CondorcetDriver.trialGenerator(numberOfMachines, machineNumber, firstTrial, numberOfTrials, baseNumPlayersPerGame, discrepancyRangeList, baseNumPlayers, numGames, timeLimitMinutes, directory, targetVar, calculatePower.printPointsToDataFile)
 
